ed3d1ad2f42bb4caeb299303e0c4962bb25d9ce1
Jeroen Reijn <j.reijn@gmail.com>
2018-10-15 12:07:28 +0200

    Remove dependency on jackson and jackson geojson.

--- src/main/java/org/openstreetmap/josm/plugins/geojson/GeoJsonLayer.java
+++ src/main/java/org/openstreetmap/josm/plugins/geojson/GeoJsonLayer.java
@@ -1,13 +1,14 @@
 // License: Apache 2.0. For details, see LICENSE file.
 package org.openstreetmap.josm.plugins.geojson;
 
-import javax.swing.Icon;
-
 import org.openstreetmap.josm.data.Bounds;
+import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
-import org.openstreetmap.josm.plugins.geojson.DataSetBuilder.BoundedDataSet;
 import org.openstreetmap.josm.tools.ImageProvider;
+
+import javax.swing.Icon;
+
 
 /**
  * Class that displays a map layer containing GeoJSON data.
@@ -18,9 +19,17 @@ import org.openstreetmap.josm.tools.ImageProvider;
 public class GeoJsonLayer extends OsmDataLayer {
     private final Bounds bounds;
 
-    public GeoJsonLayer(final String name, final BoundedDataSet data) {
-        super(data.getDataSet(), name, null);
-        this.bounds = data.getBounds();
+    /**
+     * Construct a new GeoJSON based {@code OsmDataLayer}.
+     *
+     * @param name Layer name
+     * @param data OSM data
+     * @param bounds bounds data
+     *
+     */
+    public GeoJsonLayer(final String name, final DataSet data, final Bounds bounds) {
+        super(data, name, null);
+        this.bounds = bounds;
     }
 
     public Bounds getBounds() {
