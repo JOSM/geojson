03137181391b0439ae1caf3857ce94186ccc0176
Vincent Privat <vincent.privat@gmail.com>
2019-10-05 19:42:39 +0200

    fix #josm17086 - remove useless GeoJsonDialog

--- src/main/java/org/openstreetmap/josm/plugins/geojson/GeoJsonPlugin.java
+++ src/main/java/org/openstreetmap/josm/plugins/geojson/GeoJsonPlugin.java
@@ -3,7 +3,6 @@ package org.openstreetmap.josm.plugins.geojson;
 
 import org.openstreetmap.josm.actions.ExtensionFileFilter;
 import org.openstreetmap.josm.gui.MainApplication;
-import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
@@ -22,15 +21,5 @@ public class GeoJsonPlugin extends Plugin {
         ExtensionFileFilter.addImporter(this.geoJsonFileImporter);
         ExtensionFileFilter.updateAllFormatsImporter();
         MainApplication.getMenu().openLocation.addDownloadTaskClass(GeoJsonDownloadTask.class);
-    }
-
-    @Override
-    public void mapFrameInitialized(final MapFrame oldFrame, final MapFrame newFrame) {
-        if (newFrame != null) {
-            final GeoJsonDialog dialog = new GeoJsonDialog();
-            newFrame.addToggleDialog(dialog);
-            MainApplication.getLayerManager().addLayerChangeListener(dialog);
-            MainApplication.getLayerManager().addActiveLayerChangeListener(dialog);
-        }
     }
 }
